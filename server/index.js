8b7b947c9e0581c6a32b7384e298bf063aa89f11
Hassan Sheikh <hsn24sk@gmail.com>
2020-03-08 04:48:00 +0500

    Few changes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const {app, io} = require('./setup/serverSetup');
 const {db} = require('./setup/rethinkDBSetup');
 const twilio = require('./setup/twilioSetup');
 
-const {couch, initCouch} = require('./setup/couchDBSetup');
-
 db.then(r => {
     console.log('RethinkDB has been initialized')
     require('./routes')(app,r,twilio, io);
